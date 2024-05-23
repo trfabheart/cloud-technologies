103f680fe28639008e17f235d5877923daf35c88
trfabheart <Volodymyr.Nakonechnyi.RI.2023@lpnu.ua>
2024-05-23 07:49:12 +0300

    aws notifications

--- cloudwatch.tf
+++ cloudwatch.tf
@@ -1,28 +1,40 @@
-# module "metric_alarm" {
-#   source  = "terraform-aws-modules/cloudwatch/aws//modules/metric-alarms-by-multiple-dimensions"
-#   version = "5.3.1"
+module "metric_alarm" {
+  source  = "terraform-aws-modules/cloudwatch/aws//modules/metric-alarms-by-multiple-dimensions"
+  version = "5.3.1"
 
-#   alarm_name          = "${module.label.id}-"
-#   alarm_description   = "Errors in ${module.lambdas.lambda_authors_lambda_function_name}"
-#   comparison_operator = "GreaterThanOrEqualToThreshold"
-#   evaluation_periods  = 1
-#   threshold           = 1
-#   period              = 60
-#   unit                = "Count"
-#   treat_missing_data = "ignore"
+  alarm_name          = "${module.label.id}-"
+  alarm_description   = "Errors found"
+  comparison_operator = "GreaterThanOrEqualToThreshold"
+  evaluation_periods  = 1
+  threshold           = 1
+  period              = 60
+  unit                = "Count"
+  treat_missing_data = "ignore"
 
-#   namespace   = "AWS/Lambda"
-#   metric_name = "Errors"
-#   statistic   = "Sum"
+  namespace   = "AWS/Lambda"
+  metric_name = "Errors"
+  statistic   = "Sum"
 
-#   dimensions = {
-#     "authors" = {
-#       FunctionName = "${module.lambdas.lambda_authors_lambda_function_name}"
-#     },
-#     "courses" = {
-#       FunctionName = "${module.lambdas.lambda_courses_lambda_function_name}"
-#     },
-#   }
+  dimensions = {
+    "authors" = {
+      FunctionName = "${module.lambdas.lambda_authors_lambda_function_name}"
+    },
+    "courses" = {
+      FunctionName = "${module.lambdas.lambda_courses_lambda_function_name}"
+    },
+    "get-course" = {
+      FunctionName = "${module.lambdas.lambda_get_course_lambda_function_name}"
+    },
+    "save-course" = {
+      FunctionName = "${module.lambdas.lambda_save_course_lambda_function_name}"
+    },
+    "update-course" = {
+      FunctionName = "${module.lambdas.lambda_update_course_lambda_function_name}"
+    },
+    "delete-course" = {
+      FunctionName = "${module.lambdas.lambda_delete_course_lambda_function_name}"
+    }
+  }
 
-#   alarm_actions = [module.notify_slack.slack_topic_arn]
-# }
+  alarm_actions = [module.notify_slack.slack_topic_arn]
+}
--- variables.tf
+++ variables.tf
@@ -8,6 +8,10 @@ variable "use_locals" {
     # default = "471112639379-my-tf-test-bucket"
 }
 
-# variable "slack_webhook_url" {
-#     type = string
-# }
+variable "slack_webhook_url" {
+  type = string
+}
+
+variable "slack_email" {
+  type = string
+}
